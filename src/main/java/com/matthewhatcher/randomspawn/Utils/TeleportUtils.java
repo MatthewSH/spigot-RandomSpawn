99fe8b6a4d6c5fb77d3e30eb305c2e85026be9d7
Matthew Hatcher <hello@matthewh.in>
2016-11-16 12:50:56 -0500

    Adding teleportation utilities.

---
+++ src/main/java/com/matthewhatcher/randomspawn/Utils/TeleportUtils.java
@@ -0,0 +1,27 @@
+package com.matthewhatcher.randomspawn.Utils;
+
+import java.util.Random;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import com.matthewhatcher.randomspawn.RandomSpawn;
+
+public class TeleportUtils {
+	public static void teleport(Player p) {
+		p.teleport(randomLocation(p.getWorld()));
+	}
+	
+	public static Location randomLocation(World w) {
+		FileConfiguration c = RandomSpawn.getInstance().getConfig();
+		Random r = new Random();
+		
+		int x = c.getInt("coods.x.min") + r.nextInt(c.getInt("coords.x.max") - c.getInt("coords.x.min") + 1);
+		int z = c.getInt("coods.z.min") + r.nextInt(c.getInt("coords.z.max") - c.getInt("coords.z.min") + 1);
+		int y = w.getHighestBlockYAt(x, z);
+		
+		return new Location(w, x, y, z);
+	}
+}
