a6d9358cf30f8f43084f339b26fd51e946d0ad52
Matthew Hatcher <matthewhatcher1@gmail.com>
2017-01-16 04:41:54 -0500

    Adding bed spawn, and fixing the respawn random spawn.
    Closing issue #3 and fixing the other issue where it would not teleport the user to a random location on respawn.

--- src/main/java/com/matthewhatcher/randomspawn/Listeners/RespawnListener.java
+++ src/main/java/com/matthewhatcher/randomspawn/Listeners/RespawnListener.java
@@ -4,12 +4,16 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
+import com.matthewhatcher.randomspawn.RandomSpawn;
 import com.matthewhatcher.randomspawn.Utils.TeleportUtils;
 
 public class RespawnListener implements Listener {
 	
 	@EventHandler
 	public void onRespawn(PlayerRespawnEvent e) {
-		TeleportUtils.teleport(e.getPlayer());
+		if(RandomSpawn.getInstance().getConfig().getBoolean("use-beds") && e.isBedSpawn())
+			return;
+		
+		e.setRespawnLocation(TeleportUtils.randomLocation(e.getPlayer().getWorld()));
 	}
 }
