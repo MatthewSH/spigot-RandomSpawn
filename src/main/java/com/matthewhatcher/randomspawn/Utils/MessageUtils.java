7d954a2136516de385133290cb27d6fdaecb5f24
Matthew Hatcher <matthewhatcher1@gmail.com>
2017-01-17 03:04:48 -0500

    Adding two new configuration options.

--- src/main/java/com/matthewhatcher/randomspawn/Utils/MessageUtils.java
+++ src/main/java/com/matthewhatcher/randomspawn/Utils/MessageUtils.java
@@ -3,16 +3,28 @@ package com.matthewhatcher.randomspawn.Utils;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.matthewhatcher.randomspawn.RandomSpawn;
+
 public class MessageUtils {
 	private static String prefix = "&c[Random Spawn] &b";
+	private static boolean displayMessages = true;
+	
+	public MessageUtils() {
+		MessageUtils.prefix = RandomSpawn.getInstance().getConfig().getString("chat-prefix", "&c[Random Spawn] &b");
+		MessageUtils.displayMessages = RandomSpawn.getInstance().getConfig().getBoolean("display-messages", true);
+	}
 	
 	public static void send(Player p, String m) {
-		String message = prefix + m;
-		p.sendMessage(message.replaceAll("&", "§"));
+		if(MessageUtils.displayMessages) {
+			String message = prefix + m;
+			p.sendMessage(message.replaceAll("&", "§"));
+		}
 	}
 	
 	public static void send(CommandSender s, String m) {
-		String message = prefix + m;
-		s.sendMessage(message.replaceAll("&", "§"));
+		if(MessageUtils.displayMessages) {
+			String message = prefix + m;
+			s.sendMessage(message.replaceAll("&", "§"));
+		}
 	}
 }
