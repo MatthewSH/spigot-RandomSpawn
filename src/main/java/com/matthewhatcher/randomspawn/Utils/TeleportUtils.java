962ac7cb75df9ec502e3ea5b84f7c2a15b39aad8
Matthew Hatcher <matthewhatcher1@gmail.com>
2017-03-12 20:38:54 -0400

    This adds the feature from issue #7.

--- src/main/java/com/matthewhatcher/randomspawn/Utils/TeleportUtils.java
+++ src/main/java/com/matthewhatcher/randomspawn/Utils/TeleportUtils.java
@@ -2,6 +2,7 @@ package com.matthewhatcher.randomspawn.Utils;
 
 import java.util.concurrent.ThreadLocalRandom;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -23,6 +24,20 @@ public class TeleportUtils {
 	
 	public static Location randomLocation(World w) {
 		FileConfiguration c = RandomSpawn.getInstance().getConfig();
+		
+		if(c.getBoolean("use-as-whitelist", false)) {
+			if(!c.getStringList("world-blacklist.worlds").contains(w.getName())) {
+				if(Bukkit.getWorld(c.getString("world-blacklist.fallback-world")) != null) {
+					w = Bukkit.getWorld(c.getString("world-blacklist.fallback-world"));
+				}
+			}
+		} else {
+			if(c.getStringList("world-blacklist.worlds").contains(w.getName())) {
+				if(Bukkit.getWorld(c.getString("world-blacklist.fallback-world")) != null) {
+					w = Bukkit.getWorld(c.getString("world-blacklist.fallback-world"));
+				}
+			}
+		}
 		
 		int x = generateInt(c.getInt("coords.x.min"), c.getInt("coords.x.max"));
 		int z = generateInt(c.getInt("coords.z.min"), c.getInt("coords.z.max"));
